ad2cb1fc0d459ccc058207bbdfa900230af31e38
Talin <viridia@gmail.com>
2017-09-19 22:11:17 -0700

    auto-scrolling

--- src/components/graph/GraphView.tsx
+++ src/components/graph/GraphView.tsx
@@ -1,3 +1,4 @@
+import bind from 'bind-decorator';
 import { action } from 'mobx';
 import { Component, h } from 'preact';
 import Graph from '../../graph/Graph';
@@ -9,18 +10,45 @@ interface Props {
   graph: Graph;
 }
 
-export default class GraphView extends Component<Props, undefined> {
-  public render({ graph }: Props): any {
+interface State {
+  xScroll: number;
+  yScroll: number;
+}
+
+export default class GraphView extends Component<Props, State> {
+  private scrollEl: HTMLElement;
+
+  constructor() {
+    super();
+    this.state = {
+      xScroll: 0,
+      yScroll: 0,
+    };
+  }
+
+  public render({ graph }: Props, { xScroll, yScroll }: State): any {
     return (
-      <section id="graph" onClick={this.onClick}>
-        {graph.nodes.map(node => (<NodeRendition node={node} graph={graph} />))}
+      <section id="graph" onMouseDown={this.onMouseDown}>
+        <div
+          className="scroll"
+          style={{ left: `${xScroll}px`, top: `${yScroll}px` }}
+          ref={el => { this.scrollEl = el as HTMLElement; }}
+        >
+          {graph.nodes.map(node => (
+              <NodeRendition node={node} graph={graph} onScroll={this.onScroll} />))}
+        </div>
       </section>
     );
   }
 
   @action.bound
-  private onClick(e: MouseEvent) {
+  private onMouseDown(e: MouseEvent) {
     e.preventDefault();
     this.props.graph.clearSelection();
+  }
+
+  @bind
+  private onScroll(dx: number, dy: number) {
+    this.setState({ xScroll: this.state.xScroll + dx, yScroll: this.state.yScroll + dy });
   }
 }
--- src/components/graph/NodeRendition.tsx
+++ src/components/graph/NodeRendition.tsx
@@ -1,3 +1,4 @@
+import bind from 'bind-decorator';
 import * as classNames from 'classnames';
 import { action } from 'mobx';
 import { Component, h } from 'preact';
@@ -11,12 +12,30 @@ import './NodeRendition.scss';
 interface Props {
   node: Node;
   graph: Graph;
+  onScroll: (dx: number, dy: number) => void;
 }
 
 // TODO: drag
 /** A visual representation of a node in the graph. */
 @observer
 export default class NodeRendition extends Component<Props, undefined> {
+  private dragPointer: number = undefined;
+  private dragXOffset: number = 0;
+  private dragYOffset: number = 0;
+  private body: HTMLElement;
+  private hScroll: number = 0;
+  private vScroll: number = 0;
+  private scrollTimer: number = undefined;
+
+  public componentDidMount() {
+    this.body.addEventListener('pointerdown', this.onPointerDown);
+  }
+
+  public componentWillUnmount() {
+    this.releasePointer();
+    this.body.removeEventListener('pointerdown', this.onPointerDown);
+  }
+
   public render({ node }: Props): any {
     const style = {
       left: `${node.x}px`,
@@ -25,7 +44,11 @@ export default class NodeRendition extends Component<Props, undefined> {
 
     return (
       <div className={classNames('node', { selected: node.selected })} style={style}>
-        <div className="body" onClick={this.onClick}>
+        <div
+            className="body"
+            onMouseDown={this.onMouseDown}
+            ref={el => { this.body = el as HTMLElement; }}
+        >
           <header>{node.name}</header>
           <section className="preview">
             <RenderedImage width={80} height={80} node={node} />
@@ -39,8 +62,13 @@ export default class NodeRendition extends Component<Props, undefined> {
     );
   }
 
+  @bind
+  private onMouseDown(e: MouseEvent) {
+    e.preventDefault();
+  }
+
   @action.bound
-  private onClick(e: MouseEvent) {
+  private onPointerDown(e: PointerEvent) {
     e.preventDefault();
     e.stopPropagation();
     const { node, graph } = this.props;
@@ -51,6 +79,75 @@ export default class NodeRendition extends Component<Props, undefined> {
         graph.clearSelection();
       }
       node.selected = true;
+    }
+
+    if (node.selected && this.dragPointer === undefined) {
+      const scrollEl = this.base.parentNode as HTMLElement;
+      this.dragXOffset = e.clientX - node.x - scrollEl.offsetLeft;
+      this.dragYOffset = e.clientY - node.y - scrollEl.offsetTop;
+      this.body.addEventListener('pointermove', this.onPointerMove);
+      this.body.addEventListener('pointerup', this.onPointerUp);
+      this.body.setPointerCapture(e.pointerId);
+      this.dragPointer = e.pointerId;
+    }
+  }
+
+  @action.bound
+  private onPointerMove(e: PointerEvent) {
+    const { node } = this.props;
+    const scrollEl = this.base.parentNode as HTMLElement;
+    const graphEl = scrollEl.parentNode as HTMLElement;
+    node.x = Math.min(
+      graphEl.offsetLeft + graphEl.offsetWidth,
+      Math.max(graphEl.offsetLeft, e.clientX)) - this.dragXOffset - scrollEl.offsetLeft;
+    node.y = Math.min(
+      graphEl.offsetTop + graphEl.offsetHeight,
+      Math.max(graphEl.offsetTop, e.clientY)) - this.dragYOffset - scrollEl.offsetTop;
+
+    let hScroll = 0;
+    if (e.clientX < graphEl.offsetLeft) {
+      hScroll = -1;
+    } else if (e.clientX > graphEl.offsetLeft + graphEl.offsetWidth) {
+      hScroll = 1;
+    }
+
+    let vScroll = 0;
+    if (e.clientY < graphEl.offsetTop) {
+      vScroll = -1;
+    } else if (e.clientY > graphEl.offsetTop + graphEl.offsetHeight) {
+      vScroll = 1;
+    }
+
+    if (hScroll !== 0 || vScroll !== 0) {
+      if (this.scrollTimer === undefined) {
+        this.scrollTimer = window.setInterval(() => {
+          this.props.onScroll(-hScroll * 10, -vScroll * 10);
+        }, 16);
+      }
+    } else if (this.scrollTimer !== undefined) {
+      window.clearInterval(this.scrollTimer);
+      this.scrollTimer = undefined;
+    }
+    this.hScroll = hScroll;
+    this.vScroll = vScroll;
+  }
+
+  @bind
+  private onPointerUp(e: PointerEvent) {
+    this.releasePointer();
+  }
+
+  @bind
+  private releasePointer() {
+    if (this.dragPointer !== undefined) {
+      this.body.releasePointerCapture(this.dragPointer);
+      this.body.removeEventListener('pointerup', this.onPointerUp);
+      this.body.removeEventListener('pointermove', this.onPointerMove);
+      this.dragPointer = undefined;
+    }
+    if (this.scrollTimer !== undefined) {
+      window.clearInterval(this.scrollTimer);
+      this.scrollTimer = undefined;
     }
   }
 }
